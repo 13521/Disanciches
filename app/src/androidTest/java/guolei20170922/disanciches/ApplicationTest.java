109be3f32a4404347d83f8e76815688db5a32a7f
13521 <1845772268@qq.com>
2017-10-31 13:28:54 +0800

    Initial commit

---
+++ app/src/androidTest/java/guolei20170922/disanciches/ApplicationTest.java
@@ -0,0 +1,13 @@
+package guolei20170922.disanciches;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
